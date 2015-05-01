6cab32160fc2a0bcc619709a3760e96d487a5ba2
Vinicius Carvalho <viniciusccarvalho@gmail.com>
2015-05-01 14:25:14 -0400

    Making GET methods nonsecured, everything else is

--- springbox-api-gateway/src/main/java/io/springbox/apigateway/SpringboxApiGatewayApplication.java
+++ springbox-api-gateway/src/main/java/io/springbox/apigateway/SpringboxApiGatewayApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.security.oauth2.sso.EnableOAuth2Sso;
 import org.springframework.cloud.security.oauth2.sso.OAuth2SsoConfigurer;
 import org.springframework.cloud.security.oauth2.sso.OAuth2SsoConfigurerAdapter;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.web.csrf.CsrfFilter;
@@ -50,7 +51,8 @@ public class SpringboxApiGatewayApplication  {
         @Override
         public void configure(HttpSecurity http) throws Exception {
             http.logout().and().antMatcher("/**").authorizeRequests()
-                    .antMatchers("/index.html", "/home.html", "/", "/login", "/beans", "/catalog/**").permitAll()
+                    .antMatchers("/index.html", "/home.html", "/", "/login", "/beans").permitAll()
+                    .antMatchers(HttpMethod.GET, "/recommendations/**","/people/**","/movie/**","/likes/**").permitAll()
                     .anyRequest().authenticated().and().csrf()
                     .csrfTokenRepository(csrfTokenRepository()).and()
                     .addFilterBefore(new RequestContextFilter(), HeaderWriterFilter.class)
