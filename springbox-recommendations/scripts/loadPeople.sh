f6e53712b751f8434f4b148289a1e4d39481689d
Adrian Ursu <Adrian.Ursu@opencredo.com>
2015-11-03 10:50:37 +0000

    This fixes spring-cloud-samples/springbox-cloud#9 by getting a valid access token from the Auth server before making the requests.

--- springbox-recommendations/scripts/loadPeople.sh
+++ springbox-recommendations/scripts/loadPeople.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-ROUTE=${ROUTE:-localhost:8082}
-curl ${ROUTE}/people -X POST -d '{"userName":"mstine","firstName":"Matt","lastName":"Stine"}' -H "Content-Type: application/json"
-curl ${ROUTE}/people -X POST -d '{"userName":"starbuxman","firstName":"Josh","lastName":"Long"}' -H "Content-Type: application/json"
-curl ${ROUTE}/people -X POST -d '{"userName":"littleidea","firstName":"Andrew","lastName":"Shafer"}' -H "Content-Type: application/json"
+TOKEN="$1"
+ROUTE="$2:$3"
+curl ${ROUTE}/people -X POST -d '{"userName":"mstine","firstName":"Matt","lastName":"Stine"}' -H "Content-Type: application/json" -H "Authorization: Bearer ${TOKEN}"
+curl ${ROUTE}/people -X POST -d '{"userName":"starbuxman","firstName":"Josh","lastName":"Long"}' -H "Content-Type: application/json" -H "Authorization: Bearer ${TOKEN}"
+curl ${ROUTE}/people -X POST -d '{"userName":"littleidea","firstName":"Andrew","lastName":"Shafer"}' -H "Content-Type: application/json" -H "Authorization: Bearer ${TOKEN}"
